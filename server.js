eaf1432b57dfb0dcd5b2fa36ce641006fc5f7454
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-08-03 15:43:01 -0600

    heroku updates

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-const app = express();
 
 app.use(logger("dev"));
 
@@ -16,11 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+  
+//   useUnifiedTopology: true
+// });
 
 // routes
 app.use(require("./routes/api.js"));
